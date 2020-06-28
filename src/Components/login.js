4e0fe16048f077d91e2357b7c84e52543f8ecc8e
shassaan <syedhassaan.103@gmail.com>
2020-06-29 00:07:38 +0500

    added wanrning

--- src/Components/login.js
+++ src/Components/login.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Form, Input, Button, Checkbox,Card } from 'antd';
+import { Form, Input, Button, Checkbox,Card,Grid } from 'antd';
 
 const layout = {
     labelCol: {
