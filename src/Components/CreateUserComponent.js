13e0f07940197c3a9bdb33d0162596493ccb0039
shassaan <syedhassaan.103@gmail.com>
2020-06-29 20:42:50 +0500

    Removed extra spaces and added indentaions

--- src/Components/CreateUserComponent.js
+++ src/Components/CreateUserComponent.js
@@ -1,44 +1,41 @@
 import React from 'react'
-import { Input, Button, Checkbox, Row, Col} from 'antd';
-import {Link} from 'react-router-dom'; 
+import { Input, Button, Checkbox, Row, Col } from 'antd';
+import { Link } from 'react-router-dom';
 
-const CreateUserComponent =()=>{
+const CreateUserComponent = () => {
+  return (
+    <div style={{ width: 400, height: 100, marginTop: 100, marginLeft: 200 }}>
+      <h1>User Creation Form</h1>
+      <Input placeholder="Full Name"></Input>
+      <Input placeholder="Email"></Input>
+      <Input placeholder="Date-of-Birth"></Input>
+      <label>Access Role</label>
+      <Checkbox.Group style={{ width: '100%' }} >
+        <Row>
+          <Col span={8}>
+            <Checkbox value="A">A</Checkbox>
+          </Col>
+          <Col span={8}>
+            <Checkbox value="B">B</Checkbox>
+          </Col>
+          <Col span={8}>
+            <Checkbox value="C">C</Checkbox>
+          </Col>
+          <Col span={8}>
+            <Checkbox value="D">D</Checkbox>
+          </Col>
+          <Col span={8}>
+            <Checkbox value="E">E</Checkbox>
+          </Col>
+        </Row>
+      </Checkbox.Group>,
 
-    
-    
-    return(
-        <div style={{width:400 ,height:100,marginTop:100, marginLeft:200}}>
-<h1>User Creation Form</h1>
-            <Input placeholder="Full Name"></Input>
-            <Input placeholder="Email"></Input>
-            <Input placeholder="Date-of-Birth"></Input>
-            <label>Access Role</label>
-            <Checkbox.Group style={{ width: '100%' }} >
-    <Row>
-      <Col span={8}>
-        <Checkbox value="A">A</Checkbox>
-      </Col>
-      <Col span={8}>
-        <Checkbox value="B">B</Checkbox>
-      </Col>
-      <Col span={8}>
-        <Checkbox value="C">C</Checkbox>
-      </Col>
-      <Col span={8}>
-        <Checkbox value="D">D</Checkbox>
-      </Col>
-      <Col span={8}>
-        <Checkbox value="E">E</Checkbox>
-      </Col>
-    </Row>
-  </Checkbox.Group>,
-            
 
-        <Button type="primary" style={{float:'right'}}>Add</Button>   
-        <Link to="/layout"><Button type="primary" >Back</Button></Link>
+      <Button type="primary" style={{ float: 'right' }}>Add</Button>
+      <Link to="/layout"><Button type="primary" >Back</Button></Link>
 
-</div>
-    )
+    </div>
+  )
 }
 
 export default CreateUserComponent;
--- src/Components/Login/login.js
+++ src/Components/Login/login.js
@@ -1,48 +1,48 @@
 import React from 'react'
 import './login.css'
-import { Input, Button,Card } from 'antd';
+import { Input, Button, Card } from 'antd';
 
 
 
-const login = () =>{
+const login = () => {
 
- const handleOnClick=()=>{
-  window.location.replace('/layout')
-}
-    
+    const handleOnClick = () => {
+        window.location.replace('/layout')
+    }
 
-    return(
-        
-       
 
-<div className="site-card-border-less-wrapper">
-    <Card title="Login"  style={{ width: 400 , marginLeft :370, height:400}}>
-    <div className="login">
-            
-            <Input
-                placeholder="username"
-                
-            />
-            <Input.Password
-                placeholder="password"
-               
-            />
-            <Button 
-                type="primary" 
-                block
-               onClick={handleOnClick}                >
-                login
+    return (
+
+
+
+        <div className="site-card-border-less-wrapper">
+            <Card title="Login" style={{ width: 400, marginLeft: 370, height: 400 }}>
+                <div className="login">
+
+                    <Input
+                        placeholder="username"
+
+                    />
+                    <Input.Password
+                        placeholder="password"
+
+                    />
+                    <Button
+                        type="primary"
+                        block
+                        onClick={handleOnClick}                >
+                        login
             </Button>
+                </div>
+            </Card>
         </div>
-    </Card>
-  </div>
-           
 
-       
-       
-       
 
-        
+
+
+
+
+
 
     );
 }
