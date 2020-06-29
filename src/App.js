97ccb85763bf48b90e913d9abefced795bcedaa1
sjaveriawasti <66131686+sjaveriawasti@users.noreply.github.com>
2020-06-29 20:40:25 +0500

    User creation form (#19)
    * added some changes
    
    * removed chaeckbox
    
    * Add-Layout-Component
    
    * AddUserCreationForm
    
    * Changed names
    
    * Update LayoutMenu.js
    
    * Update and rename UserCreationForm.js to CreateUserComponent.js
    
    * Rename UsercreationForm to App.js
    
    Co-authored-by: Javeria Wasti <javerwasti16@gmail.com>
    Co-authored-by: Syed Hassaan Ahmed <syedhassaan.103@gmail.com>

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router} from "react-router-dom";
 import Route from 'react-router-dom/Route';
 import Login from './Components/Login/login';
-import MainLayout from './Components/Layout/MainLayout'
+import MainLayout from './Components/Layout/MainLayout';
+import CreateUserComponent from './Components/CreateUserComponent'
 import './App.css'
 
 function App() {
   return (
     
     <Router>
+       
        <Route path="/" exact strict component={()=>{return <Login/>}}/>
        <Route path="/layout" exact strict component={()=>{return <MainLayout/>}}/>
+       <Route path="/create-user" exact strict component={()=>{return <CreateUserComponent/>}}/>
        </Router>
      
   
